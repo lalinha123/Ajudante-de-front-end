f309f296351ccc4a2fd287563d5b0f4032b2f951
lala <mimserprimitiva@gmail.com>
2022-01-25 21:43:04 -0300

    navbar ok

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,10 +1,10 @@
+const body = document.querySelector('body');
 
 function background(){
   const div = document.createElement('div');
   const span1 = document.createElement('span');
   const span2 = document.createElement('span');
   const span3 = document.createElement('span');
-  const body = document.querySelector('body');
 
 
   div.id = 'cont-back';
@@ -20,6 +20,28 @@ function background(){
   body.appendChild(div);
 }
 
+function navbar(){
+  const nav = document.createElement('nav');
+  const link_logo = document.createElement('a');
+  const link_sobre = document.createElement('a');
+  const div_right = document.createElement('div');
+
+  link_logo.id = 'logo';
+  link_logo.href = 'index.html';
+  link_logo.innerHTML = 'AF';
+
+  link_sobre.innerHTML = 'Sobre';
+
+  div_right.classList.add('float-right');
+  div_right.appendChild(link_sobre);
+
+  nav.appendChild(link_logo);
+  nav.appendChild(div_right);
+
+  body.appendChild(nav);
+}
+
 window.addEventListener('load', function(e){
   //background();
+  navbar();
 });
