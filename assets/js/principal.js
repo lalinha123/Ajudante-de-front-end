544bd93b29d197843552aac85ab6e084b86d2112
lala <mimserprimitiva@gmail.com>
2022-01-27 21:45:00 -0300

    + div code ver.1

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,5 +1,24 @@
 const body = document.querySelector('body');
 
+function mudaCode(tipo_btn, id_btn){
+  const div_css = document.querySelector('#div-ling-conteudo-css');
+  const div_html = document.querySelector('#div-ling-conteudo-html');
+  const btn_css = document.querySelector('#btn-nav-css');
+  const btn_html = document.querySelector('#btn-nav-html');
+  const div = document.getElementById(`div-ling-conteudo-${tipo_btn}`);
+  const btn = document.getElementById(id_btn);
+
+  div_css.classList.remove('aberto');
+  div_html.classList.remove('aberto');
+  btn_css.classList.remove('aberto');
+  btn_html.classList.remove('aberto');
+
+  div.classList.add('aberto');
+  btn.classList.add('aberto');
+}
+
+
+
 function navbar(){
   const nav = document.createElement('nav');
   const link_logo = document.createElement('a');
