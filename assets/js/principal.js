270072fa2642e23691d366a436690ff342dc030b
Laís Gonçalves <mimserprimitiva@gmail.com>
2022-03-06 19:16:55 -0300

    titulo foi embora ( ' U ' )-b

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -3,17 +3,17 @@ const body = document.querySelector('body');
 function navbar(){
   const nav = document.createElement('nav');
   const link_logo = document.createElement('a');
-  const link_sobre = document.createElement('a');
+  //const link_sobre = document.createElement('a');
   const div_right = document.createElement('div');
 
   link_logo.id = 'logo';
   link_logo.href = 'index.html';
   link_logo.innerHTML = 'AF';
 
-  link_sobre.innerHTML = 'Sobre';
+  //link_sobre.innerHTML = 'Sobre';
 
   div_right.classList.add('float-right');
-  div_right.appendChild(link_sobre);
+  //div_right.appendChild(link_sobre);
 
   nav.appendChild(link_logo);
   nav.appendChild(div_right);
